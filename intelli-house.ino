cad6c21faf087a5849fe9e6de55b47a63556bf8c
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2020-10-31 18:08:16 -0600

    Main file with threads

--- intelli-house.ino
+++ intelli-house.ino
@@ -2,15 +2,17 @@
 // by David Alvarado & Erick Cordero
 
 #include <photosensor.h>
+#include <pt.h>
 
+struct pt thread1;
 
 Photosensor photosensor(A0,5);
 
 void setup() {
-
+  PT_INIT(&thread1);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  photosensor.execute();
+  photosensor.run(&thread1);
 }
