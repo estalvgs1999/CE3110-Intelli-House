79d100d1fe6c2c8019799ae13716d2999c1d5493
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2020-11-07 23:29:35 -0600

    local changes before merge

--- intelli-house.ino
+++ intelli-house.ino
@@ -1,22 +1,28 @@
+
+
 // Intelli-House
 // by David Alvarado & Erick Cordero
 #include <distanceSensor.h>
 #include <photosensor.h>
 #include <pt.h>
 #include <motionSensor.h>
+//#include <temperatureSensor.h>
 
 struct pt thread1;
 struct pt thread2;
 struct pt thread3;
+struct pt thread4;
 
 Photosensor photosensor(A0,5);
 DistanceSensor distance(6,7,10);
 MotionSensor motion(8,9);
+//Temperature tmp(12,13);
 
 void setup() {
   PT_INIT(&thread1);
   PT_INIT(&thread2);
   PT_INIT(&thread3);
+  PT_INIT(&thread4);
 }
 
 void loop() {
@@ -24,4 +30,5 @@ void loop() {
   photosensor.run(&thread1);
   distance.run(&thread2);
   motion.run(&thread3);
+  //tmp.run(&thread4);
 }
