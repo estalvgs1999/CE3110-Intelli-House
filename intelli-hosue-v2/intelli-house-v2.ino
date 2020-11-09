fed4c673d78fb5709c2e3ab724e0d68e291bfe29
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2020-11-09 15:19:02 -0600

    LDR sensor integrated. Test pending

--- intelli-hosue-v2/intelli-house-v2.ino
+++ intelli-hosue-v2/intelli-house-v2.ino
@@ -17,23 +17,53 @@
 
 #define SERVO_GARAGE 5
 #define SERVO_PT1 6
-#define SERVO_PT1 7
+#define SERVO_PT2 7
 
 // Hilos
+struct pt procesoA;
+struct pt procesoB;
+struct pt procesoC;
+struct pt procesoD;
 
 
 void setup(){
-
+  PT_INIT(&procesoA);
 }
 
 
 void loop(){
-
+  fotosensor(&procesoA);
 }
 
+/**
+ * Sensor LDR. 
+ * Si el valor de la lectura es menor que el de un valor
+ * límite (indica oscuridad) enciende la iluminación. 
+ */
 void fotosensor(struct pt *pt){
+  PT_BEGIN(pt);
+  
+  static long t = 0;
+  int valor;
+  const int valorLimite = 200;
 
+  pinMode(LED_PATIO, HIGH);
 
+  while(true){
+    digitalWrite(LED_PATIO, HIGH);
+    
+    valor = analogWrite(LDR);
+    
+    if(value < valorLimite){
+      digitalWrite(LED_PATIO, HIGH);
+    }
+    
+    t = millis();
+    PT_WAIT_WHILE(pt,((millis()-t) < 200));
+
+    PT_YIELD(pt);
+  }
+  PT_END(pt);
 }
 
 void sensorDistancia(struct pt *pt){
