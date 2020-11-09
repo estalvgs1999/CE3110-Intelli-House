1287a4447646bfe0dfcc73ac5ec235ee24197237
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2020-11-09 15:01:31 -0600

    Version 2 with all modules  integrated in a single script

---
+++ intelli-hosue-v2/intelli-house-v2.ino
@@ -0,0 +1,52 @@
+//Bibliotecas
+#include <Servo.h>
+#include <SimpleDHT.h>
+#include <pt.h>
+
+// Sensores
+#define LDR A0
+#define HC_TRIGGER 48
+#define HC_ECHO 49
+#define PIR 50
+#define DHT 53
+
+// Salidas Digitales
+#define LED_PATIO 44
+#define LED_BANO 45
+#define ACDC 46
+
+#define SERVO_GARAGE 5
+#define SERVO_PT1 6
+#define SERVO_PT1 7
+
+// Hilos
+
+
+void setup(){
+
+}
+
+
+void loop(){
+
+}
+
+void fotosensor(struct pt *pt){
+
+
+}
+
+void sensorDistancia(struct pt *pt){
+
+}
+
+void sensorMovimiento(struct pt *pt) {
+  
+}
+
+void sensorTemperatura(struct pt *pt){
+
+}
+
+/***********************/
+
