cb7d8272da15c0f4dd0f220e1db4c0f6d9f1d063
David <davidmd2054@gmail.com>
2020-11-10 18:08:24 -0600

    Cambios a segmento rfid

--- intelli-house.ino
+++ intelli-house.ino
@@ -24,11 +24,6 @@
 #define SERVO_PT1 6
 #define SERVO_PT2 7
 
-#define LED_R1 22
-#define LED_V1 23
-#define LED_R2 24
-#define LED_V2 25
-
 //Bases de datos
 String baseDatos[] = {"C7 52 6B 63","A9 A0 8B C1"};
 int bdSize = sizeof(baseDatos)/sizeof(String);
@@ -37,9 +32,13 @@ int bdSize2 = sizeof(baseDatos2)/sizeof(String);
 
 // Creación de Objetos
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+Servo servoGarage;
+SimpleDHT11 Sensor(DHT);
+
 
 // Variables globales
 volatile unsigned long LastPulseTime;  
+volatile bool servoGarageState;
 
 // Hilos
 struct pt procesoA;
@@ -47,14 +46,28 @@ struct pt procesoB;
 struct pt procesoC;
 struct pt procesoD;
 
+/**
+ * Retorno del pulso
+ * ---------------------------------------
+ * Sustituye la función síncrosa pulseIn()
+ */
+void EchoPinISR(){
+  static unsigned long t;
+
+  if(digitalRead(HC_ECHO))
+    t = micros();
+  else
+    LastPulseTime = micros() - t;
+}
+
 // Setup
 void setup(){
   PT_INIT(&procesoA);
   PT_INIT(&procesoB);
   PT_INIT(&procesoC);
+  PT_INIT(&procesoD);
 
   Serial.begin(9600);
-  
   //SPI.begin();
   //mfrc522.PCD_Init();
 }
@@ -64,6 +77,7 @@ void loop(){
   fotosensor(&procesoA);
   sensorMovimiento(&procesoB);
   sensorDistancia(&procesoC);
+  servoGarageRunTime(&procesoD);
 }
 
 /**
@@ -107,41 +121,38 @@ void fotosensor(struct pt *pt){
  */
 void sensorDistancia(struct pt *pt){
   PT_BEGIN(pt);
+  static long t = 0;
   const int delay_us = 10; // 10 microsegundos  
-  const int limite = 20; // 15 cm
+  const int limite = 100; // 15 cm
   int distancia;
   int tiempo;
-  Servo servo;
-  servo.attach(SERVO_GARAGE);
-
-  attachInterurpt(0, EchoPinISR, CHANGE); // Pin 2
-
+  
   pinMode(HC_ECHO, INPUT);
   pinMode(HC_TRIGGER, OUTPUT);
-  
+  attachInterrupt(0, EchoPinISR, CHANGE); // Pin 2
   
   while(true){
 
     digitalWrite(HC_TRIGGER, LOW);
     t = micros();
-    PT_WAIT_WHILE(pt,(micros()-t) < 5);
+    PT_WAIT_WHILE(pt,(micros()-t) < 10);
     digitalWrite(HC_TRIGGER, HIGH);
     t = micros();
-
-    PT_WAIT_WHILE(pt,(micros()-t) < delay_us);
+    PT_WAIT_WHILE(pt,(micros()-t) < 10);
     digitalWrite(HC_TRIGGER, LOW);
 
-    distancia = LastPulseTime/58.2;
-
-    if(distancia < limite)
-      servo.write(90);
-    else
-      servo.write(0);
-
+    tiempo = LastPulseTime;
+    distancia = tiempo/59;
+    
     Serial.print("Distancia = ");
     Serial.print(distancia);
     Serial.println(" cm");
     
+    if(distancia < limite)
+      servoGarageState = true;
+    else
+      servoGarageState = false;      
+
     PT_YIELD(pt);
   }
 
@@ -149,19 +160,30 @@ void sensorDistancia(struct pt *pt){
 }
 
 /**
- * Sustituye la función síncrosa pulseIn()
+ * Servo del patio
+ * -----------------------------------------------
+ * El sensor mide la distancia en cm.
+ * Cuando la distancia es menor a una distancia d
+ * se activa una bandera que mueve el sensor.
  */
-void EchoPinISR(){
+void servoGarageRunTime(struct pt *pt){
+  PT_BEGIN(pt);
+  static long t = 0;
+  const int _delay = 5000;
+  servoGarage.attach(SERVO_GARAGE);
+  servoGarage.write(120);
 
-  static unsigned long t;
-
-  if(digitalRead(HC_ECHO))
-    t = micros();
-  else
-    LastPulseTime = micros() - t;
+  while(true){
+    if(servoGarageState){
+      servoGarage.write(90);
+      t = millis();
+      PT_WAIT_WHILE(pt,(millis()-t) < _delay);
+      servoGarage.write(120);
+    }
+    PT_YIELD(pt);
+  }
+  PT_END(pt);
 }
-
-
 
 /**
  * Sensor PIR
@@ -197,7 +219,23 @@ void sensorMovimiento(struct pt *pt) {
 }
 
 void sensorTemperatura(struct pt *pt){
+  PT_BEGIN(pt);
+  static long t = 0;
+  byte Temp = 0;
+  byte Hum = 0;
 
+  while(true){
+
+    Serial.println("Datos Sensor DHT11");
+    Sensor.read(&Temp, &Hum, NULL);
+    Serial.print("Temperatura: ");
+    Serial.print(int(Temp));
+    Serial.print(" C°, ");
+    delay(1000);
+    
+    PT_YIELD(pt);
+  }
+  PT_END(pt);
 }
 
 
@@ -218,8 +256,6 @@ void RFID1(struct pt *pt){
       
       if(verificarTarjeta(uid))
         accesoPermitido();
-      else
-        accesoDenegado(); 
     }
   }  
 
@@ -244,8 +280,6 @@ void RFID2(struct pt *pt){
       
       if(verificarTarjeta2(uid))
         accesoPermitido2();
-      else
-        accesoDenegado2(); 
     }
   }  
 
@@ -257,30 +291,14 @@ void RFID2(struct pt *pt){
 
 // Enciende el led verde del sensor 1
 void accesoPermitido(){
-  digitalWrite(LED_V1,HIGH);
+  //Lo hará después
   //milis
-  digitalWrite(LED_V1,LOW);
-}
-
-// Enciende el led rojo del sensor 1
-void accesoDenegado(){
-  digitalWrite(LED_R1,HIGH);
-  //milis
-  digitalWrite(LED_R1,LOW);
 }
 
 // Enciende el led verde del sensor 2
 void accesoPermitido2(){
-  digitalWrite(LED_V2,HIGH);
+  //Lo hará después
   //milis
-  digitalWrite(LED_V2,LOW);
-}
-
-// Enciende el led rojo del sensor 2
-void accesoDenegado2(){
-  digitalWrite(LED_R2,HIGH);
-  //milis
-  digitalWrite(LED_R2,LOW);
 }
 
 // Devuelve el UID de la tarjeta
